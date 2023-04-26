da3f824dfa14b09693ff56e231b0a3f60a3a90df
João Pedro <joaopedromari28@gmail.com>
2023-04-26 11:07:35 -0300

    primeiro commit

---
+++ routes/routes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router()
+module.exports = router;
+const modeloTarefa = require('../models/tarefa');
+
+router.post('/post', async (req, res) => {
+    const objetoTarefa = new modeloTarefa({
+        descricao: req.body.descricao,
+        statusRealizada: req.body.statusRealizada
+    })
+    try {
+        const tarefaSalva = await objetoTarefa.save();
+        res.status(200).json(tarefaSalva)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.get('/getAll', async (req, res) => {
+    try {
+        const resultados = await modeloTarefa.find();
+        res.json(resultados)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const resultado = await modeloTarefa.findByIdAndDelete(req.params.id)
+        res.json(resultado)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const novaTarefa = req.body;
+        const options = { new: true };
+        const result = await modeloTarefa.findByIdAndUpdate(
+            id, novaTarefa, options
+        )
+        res.json(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
